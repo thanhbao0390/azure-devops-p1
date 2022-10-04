5598c2ecbe54cb23d088b4edfdb2d4340bc57653
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2022-10-04 19:05:02 +0700

    add

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default     = "udacity"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "East US"
+}
+
+variable "username" {
+  description = "username."
+}
+
+variable "password" {
+  description = "password."
+}
